33285870ab8ea7f2b4659e40c7b556849b3813c4
MASLO5343 <88845296+MASLO5343@users.noreply.github.com>
2025-05-19 03:19:40 +0300

    ывывыв

---
+++ project wpf/WpfApp1/Pages/TicketsPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using WpfApp1.ViewModels; // Добавьте using для ViewModel
+
+namespace WpfApp1.Pages
+{
+    public partial class TicketsPage : Page // Строка 6 (для CS8646)
+    {
+        public TicketsPage(TicketsViewModel viewModel)
+        {
+            InitializeComponent(); // Строка 10 (для CS0121)
+            DataContext = viewModel;
+        }
+    }
+}
